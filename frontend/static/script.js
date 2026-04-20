5bacf1d6f65b82fbb4b6434a692eb5daffe48ca1
Manuel Silva <manuul.1188@gmail.com>
2026-04-19 18:58:02 -0600

    correccion de rutas

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -24,8 +24,6 @@ const startDate = new Date("2025-04-12T14:10:00");
 
 // Local:
 // const API_URL = "http://127.0.0.1:8000";
-
-// Auto:
 const API_URL =
   window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
     ? "http://127.0.0.1:8000"
