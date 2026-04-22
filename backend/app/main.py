574ad7996dc7009b828f42a0297f38598bab6d7c
Manuel Silva <manuul.1188@gmail.com>
2026-04-22 00:13:37 -0600

    fix cors for vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,15 +26,11 @@ cloudinary.config(
     secure=True,
 )
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:8000",
-        "http://localhost:8000",
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "https://jessymanuel.netlify.app",
-    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
